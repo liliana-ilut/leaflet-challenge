bd54bc92aa19e16014f3ea6d600436d4b06e47cd
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-11-01 13:38:30 -0600

    added legend title to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,10 +4,17 @@ var map = L.map("map-id", {
     
   });
   
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  //   maxZoom: 18,
+  //   id: "light-v10",
+  //   accessToken: API_KEY
+  // }).addTo(map);
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "light-v10",
+    id: "satellite-v9",
     accessToken: API_KEY
   }).addTo(map);
 
@@ -45,7 +52,7 @@ var map = L.map("map-id", {
       style: markerInfo,
       onEachFeature: function(feature, layer) {
         layer.bindPopup(
-          "<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p>Magnitude: " + feature.properties.mag + "</p>"
+          "<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p>Richter magnitude scale: " + feature.properties.mag + "</p>"
         );
       }
     }).addTo(map)
@@ -67,26 +74,51 @@ var map = L.map("map-id", {
     };
 };
 // create legend
-    var legend = L.control({
-      position: "bottomleft"
-    });
+  //   var legend = L.control({
+  //     position: "bottomleft"
+  //   });
   
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "legend");
+  //   legend.onAdd = function() {
+  //     var div = L.DomUtil.create("div", "legend");
   
-      var grades = [0,1,2,3,4,5]
-      var colors = ["#ADFF2F","#9ACD32","#FFFF00","#ffd700","#FFA500","#FF0000"]
+  //     var grades = [0,1,2,3,4,5]
+  //     var colors = ["#ADFF2F","#9ACD32","#FFFF00","#ffd700","#FFA500","#FF0000"]
       
-      for (var i = 0; i < grades.length; i++) {
+      
+  //     for (var i = 0; i < grades.length; i++) {
   
-              div.innerHTML +=
-                "<i style = 'background: " + colors[i] + "'></i> " +
-                grades[i] + (grades[i+1] ? "&ndash;" + grades[i+1] + "<br>" : "+");
+  //             div.innerHTML +=
+  //               "<i style = 'background: " + colors[i] + "'></i> " +
+  //               grades[i] + (grades[i+1] ? "&ndash;" + grades[i+1] + "<br>" : "+");
              
-          }
+  //         }
           
-      return div;
-  };
+  //     return div;
+  // };
+  var legend = L.control({position: 'bottomleft'});  
+    legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0,1,2,3,4,5],
+        colors = ["#ADFF2F","#9ACD32","#FFFF00","#ffd700","#FFA500","#FF0000"],
+        labels = ['<strong> RICHTER SCALE </strong>'],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades [i];
+        to = grades[i+1]-1;
+
+    labels.push(
+        '<i style="background:' + colors[i] + '"></i> ' +
+        from + (to ? '&ndash;' + to : '+'));
+        }
+        div.innerHTML = labels.join('<br>');
+        return div;
+
+
+        };
+
+
     // Add the info legend to the map
     legend.addTo(map);
     
